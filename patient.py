9ceb7db0584fe4b72b584e9c63c46a20b46c4b82
Austin <winegara@gmail.com>
2021-09-12 19:38:23 -0500

    testing out patient ideas

---
+++ patient.py
@@ -0,0 +1,11 @@
+
+age = 20
+first_name = "John"
+last_name = "Smith"
+new_patient = True
+
+if new_patient == True:
+    print("New Patient")
+else:
+    print("Current Patient")
+print(first_name,",",last_name,"-", "Age:", age)
